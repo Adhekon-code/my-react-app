813a0f8d60eb3bd428e3a93b11d1f3f8a0b90da4
Adhekon-code <hellenngaukono2021@gmail.com>
2022-01-02 19:32:24 +0300

    Added  a paragraph for git hub link

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ ReactDOM.render(
   <React.StrictMode>
     <h4>What's the weather like today?</h4>
     <App />
-    
+    <p>
+      Github link :
+      <a href="https://github.com/Adhekon-code/my-react-app">
+        Open source code
+      </a>
+    </p>
   </React.StrictMode>,
   document.getElementById("root")
 );
